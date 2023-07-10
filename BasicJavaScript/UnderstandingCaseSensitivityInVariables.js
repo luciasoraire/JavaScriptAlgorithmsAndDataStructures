30663462c867ea13800c5bc72d3e62a171f0e3c5
Lucia Soraire <luciasoraire@gmail.com>
2023-07-10 19:48:49 -0300

    Comprendiendo la sensibilidad de mayúsculas en las variables

---
+++ BasicJavaScript/UnderstandingCaseSensitivityInVariables.js
@@ -0,0 +1,9 @@
+// Escribe los nombres de las variables en JavaScript en camelCase. En camelCase, los nombres de variables de múltiples palabras tienen la primera palabra en minúsculas y la primera letra de cada palabra posterior en mayúsculas.
+
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;

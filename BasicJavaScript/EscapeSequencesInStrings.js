b255031b6eea472888d3f0266a338366211b011a
Lucia Soraire <luciasoraire@gmail.com>
2023-07-31 16:18:27 -0300

    Escapa secuencias en cadenas

---
+++ BasicJavaScript/EscapeSequencesInStrings.js
@@ -0,0 +1,11 @@
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Cambia esta línea
+
+/*Código	Resultado
+\'	comilla simple
+\"	comilla doble
+\\	barra invertida
+\n	línea nueva
+\t	tabulador
+\r	retorno del carro
+\b	retroceso
+\f	fuente de formulario*/

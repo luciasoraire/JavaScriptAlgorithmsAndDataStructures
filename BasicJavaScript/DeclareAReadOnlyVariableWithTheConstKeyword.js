e6ea6182933ce8b081c0117c10acc06e6889f19f
Lucia Soraire <luciasoraire@gmail.com>
2023-07-31 14:25:17 -0300

    Declara una variable de solo lectura con la palabra clave const

---
+++ BasicJavaScript/DeclareAReadOnlyVariableWithTheConstKeyword.js
@@ -0,0 +1,6 @@
+const FCC = "freeCodeCamp"; 
+let fact = "is cool!"; 
+fact = "is awesome!";
+console.log(FCC, fact); 
+
+//const tiene todas las características increíbles que tiene let, con el bono añadido de que las variables declaradas usando const son de solo lectura. Son un valor constante, lo que significa que una vez que una variable es asignada con const, no se puede reasignar:

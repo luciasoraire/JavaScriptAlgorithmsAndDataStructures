dc70bd653d973ec8b267cdda55a517f1028eb61b
Lucia Soraire <luciasoraire@gmail.com>
2023-07-31 15:58:15 -0300

    Escapa comillas literales en cadenas

---
+++ BasicJavaScript/EscapingLiteralQuotesInStrings.js
@@ -0,0 +1,6 @@
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Cambia esta línea
+
+/*En JavaScript, puedes escapar una comilla de considerarse un final de cadena colocando una barra invertida (\) delante de la comilla.
+
+const sampleStr = "Alan said, \"Peter is learning JavaScript\".";
+Esto indica a JavaScript que la siguiente comilla no es el final de la cadena, sino que debería aparecer dentro de la cadena. Así que si imprimieras esto en la consola, obtendrías:*/

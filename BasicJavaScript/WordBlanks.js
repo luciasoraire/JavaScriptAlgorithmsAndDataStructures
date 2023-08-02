82b179675cc7736e813cdea608bbb94d8bcbd5bc
Lucia Soraire <luciasoraire@gmail.com>
2023-08-02 12:34:40 -0300

    Palabra en blanco

---
+++ BasicJavaScript/WordBlanks.js
@@ -0,0 +1,7 @@
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Cambia solo el código debajo de esta línea
+const wordBlanks = "My " + myNoun + " is " + myAdjective + " and it " + myVerb + " very " + myAdverb; 

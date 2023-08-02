771959e34a4567403eb8a4ca533057979b40e5c9
Lucia Soraire <luciasoraire@gmail.com>
2023-08-02 12:49:42 -0300

    Accede a los datos de un arreglo con índices

---
+++ BasicJavaScript/AccessArrayDataWithIndexes.js
@@ -0,0 +1,4 @@
+/*Los índices de los arreglos se escriben en la misma notación de corchetes que usan las cadenas de texto, con la excepción que en vez de especificar un carácter, se especifica una entrada en el arreglo. Así como las cadenas de texto, los arreglos usan indexación empezando desde cero, en donde el primer elemento de un arreglo tiene como índice 0.*/
+const myArray = [50, 60, 70];
+
+const myData = myArray[0];

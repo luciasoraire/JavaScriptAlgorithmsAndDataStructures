76d51b884e50060fef782e101df4c94625313dd0
Lucia Soraire <luciasoraire@gmail.com>
2023-08-30 18:22:15 -0300

    Modifica los datos de un arreglo con índices

---
+++ BasicJavaScript/ModifyArrayDataWithIndexes.js
@@ -0,0 +1,7 @@
+// Configuración
+const myArray = [18, 64, 99];
+
+// Cambia solo el código debajo de esta línea
+myArray[0] = 45;
+
+//No debe haber espacios entre el nombre del arreglo y los corchetes, como array [0]. Aunque JavaScript pueda procesar esto correctamente, puedes confundir a otros programadores al leer tu código.

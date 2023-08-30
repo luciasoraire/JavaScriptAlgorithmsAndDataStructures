a8283f8fe1b953369b74806ca9ec42d9a8dfb965
Lucia Soraire <luciasoraire@gmail.com>
2023-08-30 19:01:16 -0300

    Pasa valores a las funciones utilizando argumentos

---
+++ BasicJavaScript/PassingValuesToFunctionsWithArguments.js
@@ -0,0 +1,7 @@
+/*Los parámetros son variables que actúan como marcadores de posición para los valores que deben ser introducidos en una función cuando se llama. Cuando se define una función, se define típicamente junto con uno o más parámetros. Los valores reales que son introducidos (o "pasados") a una función cuando se llama son conocidos como argumentos.*/
+
+function functionWithArgs(a,b){
+    console.log(a+b);
+  }
+  
+  functionWithArgs(4,5);

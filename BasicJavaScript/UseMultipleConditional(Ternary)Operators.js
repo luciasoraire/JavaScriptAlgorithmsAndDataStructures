51e83a77f3b4835094095b378ed0bf6dc4c21acc
Lucia Soraire <luciasoraire@gmail.com>
2024-02-16 21:00:51 -0300

    Usa múltiples operadores condicionales (ternarios)

---
+++ BasicJavaScript/UseMultipleConditional(Ternary)Operators.js
@@ -0,0 +1,34 @@
+/*En el desafío anterior, usaste un único operador condicional. También puedes encadenarlos para comprobar múltiples condiciones.
+
+La siguiente función utiliza sentencias if, else if, y else para comprobar múltiples condiciones:
+
+function findGreaterOrEqual(a, b) {
+  if (a === b) {
+    return "a and b are equal";
+  }
+  else if (a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+La función anterior puede ser reescrita utilizando múltiples operadores condicionales:
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" 
+    : (a > b) ? "a is greater" 
+    : "b is greater";
+}
+Se considera buena práctica dar formato a múltiples operadores condicionales de forma que cada condición esté en una línea separada, como se muestra arriba. Usar múltiples operadores condicionales sin una indentación adecuada puede hacer tu código difícil de leer. Por ejemplo:
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
+En la función checkSign, usa múltiples operadores condicionales (siguiendo el formato recomendado usado en findGreaterOrEqual) para comprobar si un número es positivo, negativo o cero. La función debe devolver positive, negative o zero.*/
+
+function checkSign(num) {
+    return (num<0) ? "negative" : (num===0) ? "zero" : "positive"
+  }
+  
+  checkSign(10);
